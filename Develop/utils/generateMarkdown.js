918dbf79f750f3f389afc5d7b66c8f2495f0986c
goingblindbro <NOHRENTERPRISE@GMAIL.COM>
2022-05-21 14:05:30 -0700

    generated readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -76,7 +76,7 @@ ${renderLicenseBadge(data.license)}
   
   ## Questions
   
-  If you have any questions about the repo, open an issue or contact me directly at ${
+  Please reach out with any questions at ${
       data.email
     }. You can find more of my work at [${data.github}](https://github.com/goingblindbro/${
       data.github
